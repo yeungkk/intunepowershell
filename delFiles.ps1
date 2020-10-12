28fd3389b800e761fb08914cd1e55de8449a141a
yeungkk <72717466+yeungkk@users.noreply.github.com>
2020-10-12 09:41:12 +0800

    Update delFiles.ps1

--- delFiles.ps1
+++ delFiles.ps1
@@ -1,4 +1,4 @@
-$desktopFolder = $env:USERPROFILE + "\Desktop"
-$downloadFolder = $env:USERPROFILE + "\Downloads\*"
+$desktopFolder = $env:USERPROFILE + "\Desktop\testing"
+$downloadFolder = $env:USERPROFILE + "\Downloads\testing\*"
 Remove-Item $desktopFolder -Exclude *.lnk 
 Remove-Item $downloadFolder
